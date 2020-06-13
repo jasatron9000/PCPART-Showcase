f1ee56b9e70d8182b7a64957eb93a8624374bf40
Jason Buquiran <jbuq319@UoA.auckland.ac.nz>
2020-06-13 20:20:58 +1200

    -Revised Login Activity  -Added Login Functionality  -Added Register Functionality

--- app/src/main/java/com/example/pcpartsoftware/LoginActivity.java
+++ app/src/main/java/com/example/pcpartsoftware/LoginActivity.java
@@ -2,13 +2,62 @@ package com.example.pcpartsoftware;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    //View in the Activity
+    private EditText usernameInfo;
+    private EditText passwordInfo;
+    private Button loginButton;
+    private Button registerButton;
+
+    private DataProvider dp = DataProvider.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        //Initialise the view class with their corresponding elements in the XML
+        usernameInfo = findViewById(R.id.username);
+        passwordInfo = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login);
+        registerButton = findViewById(R.id.register);
+
+        //Event handling for the login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameInfo.getText().toString();
+                String password = passwordInfo.getText().toString();
+
+                if(dp.getcDB().checkCredentials(username, password)){
+                    Toast.makeText(v.getContext(), "Access Granted", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(v.getContext(), "Wrong Username or Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //Event handling for the login button
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Go to the register activity
+                gotoRegisterActivity();
+            }
+        });
+
+    }
+
+    public void gotoRegisterActivity(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/pcpartsoftware/RegisterActivity.java
+++ app/src/main/java/com/example/pcpartsoftware/RegisterActivity.java
@@ -2,13 +2,68 @@ package com.example.pcpartsoftware;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
+    private ArrayList<EditText> listOfFields = new ArrayList<>();
+
+    private Button registerButton;
+    private Button loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+
+        Intent currentIntent = getIntent();
+
+        //Intialise the fields
+        initialiseFields();
+
+        registerButton = findViewById(R.id.register2);
+        loginButton = findViewById(R.id.login2);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Check if any of the fields are empty
+                for (EditText fields : listOfFields){
+                    if(fields.getText().toString().equals("")){
+                        Toast.makeText(RegisterActivity.this, "Do not leave any fields blank.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoLoginActivity();
+            }
+        });
+    }
+
+    private void gotoLoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void initialiseFields(){
+        int[] ids = new int[]{R.id.first_name, R.id.last_name, R.id.email,
+                R.id.phoneNumber, R.id.address, R.id.username, R.id.password};
+
+        for (int id : ids) {
+            listOfFields.add((EditText) findViewById(id));
+        }
     }
 }
